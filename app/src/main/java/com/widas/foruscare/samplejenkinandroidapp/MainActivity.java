08a26fdad200dc6657f1dd87325ca3124123c826
giri <giridharan.s@widas.in>
2016-01-26 16:16:23 +0530

    toast message added

--- app/src/main/java/com/widas/foruscare/samplejenkinandroidapp/MainActivity.java
+++ app/src/main/java/com/widas/foruscare/samplejenkinandroidapp/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Toast.makeText(getApplication(),"Welcome to jenkins Android",Toast.LENGTH_LONG).show();
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
